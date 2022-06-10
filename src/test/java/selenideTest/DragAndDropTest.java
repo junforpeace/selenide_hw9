02b554e450f8eff5c2a1a5296e2c13c90fcd501a
junforpeace <bqsoapcream@gmail.com>
2022-06-10 17:19:45 +0200

    DragAndDropTest

---
+++ src/test/java/selenideTest/DragAndDropTest.java
@@ -0,0 +1,20 @@
+package selenideTest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @Test
+    void dragAtoPlaceB() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
+    }
+}
